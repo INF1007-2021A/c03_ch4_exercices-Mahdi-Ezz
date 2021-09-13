96bb57ceaefe073d0c1a86c1328e622a664bb5d7
Mahdi-Ezz <89825337+Mahdi-Ezz@users.noreply.github.com>
2021-09-13 14:20:16 -0400

    mahdi.ezzedine@polymtl.ca

--- exercice.py
+++ exercice.py
@@ -3,27 +3,42 @@
 
 
 def is_even_len(string: str) -> bool:
-    return False
+    if len(string)%2==0:
+        return True
 
 
 def remove_third_char(string: str) -> str:
-    return ""
+    return string.replace(string[2],'')
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+    return string.replace('w','z')
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
+    nbre=0
+    for letter in (string):
+     if letter== char:
+        nbre +=1
+    return nbre
 
 
-def get_nb_words(sentence: str) -> int:
+def get_nb_words(string: str) -> int:
+    nbre=1
+    if string=='':
+        nbre = 0
+    else:
+        for letter in (string):
+            if letter==' ':
+                nbre +=1
+    return nbre
+    
     return 0
 
 
 def main() -> None:
     string = "Bonjour!"
+    print(len(string))
     parity = 'pair' if is_even_len(string) else 'impair'
     print(f"Le nombre de caractère dans la chaine '{string}' est {parity}")
 
@@ -32,7 +47,7 @@ def main() -> None:
 
     string = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
-
+    string= "hello"
     print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
     
     string = "Baby shark doo doo doo doo doo doo"
